1ae4da3d4327859d33e79c8b46b3134c64761769
Hedie <97803045+mistysky04@users.noreply.github.com>
2022-11-07 11:38:56 -0500

    Add files via upload
    Completed flappy bird game with full functionality

--- Code/flappybird.py
+++ Code/flappybird.py
@@ -2,14 +2,17 @@ import pygame
 from sys import exit
 import random
 
-#Constant variables
+#CONSTANT VARIABLES
 pipe_distance = 720
 speed = 5
+gravity = 0
 
-#Classes & Functions
+#CLASSES
 class Player(pygame.sprite.Sprite):
     def __init__(self):
+        #Inherit properties of sprite class
         super().__init__()
+        #Import bird pictures & scale
         bird_1 = pygame.image.load('desktop/Pygamefiles/Flappy Bird/Graphics/Bird/Bird 4.png').convert_alpha()
         bird_1 = pygame.transform.scale(bird_1, (70, 30))
 
@@ -22,7 +25,7 @@ class Player(pygame.sprite.Sprite):
         
         self.image = self.frames[self.bird_index]
         self.rect = self.image.get_rect(center = (50,300))
-        self.gravity = 0
+        self.gravity = gravity
     
     def player_input(self):
         #Gives all inputs for keys pressed 
@@ -31,21 +34,24 @@ class Player(pygame.sprite.Sprite):
             #How high each button press will cause the bird to "jump" 
             #larger (-) value = jumps higher 
             #DO NOT make it +=, it will cause the bird to move REALLY WACK
-            self.gravity = -4.5
+            self.gravity = -5.5
     
     def apply_gravity(self):
         #How fast player falls (i.e. we want them to fall slightly slower for easier gameplay)
         self.gravity += 0.7
         self.rect.y += self.gravity
+        #Ensure player does not go beyond screen bounds
         if self.rect.bottom >= 580:
             self.rect.bottom = 580
     
     def animation(self):
+        #Set animation rate & loop between images
         self.bird_index += 0.1
         if self.bird_index >= len(self.frames): self.bird_index = 0
         self.image = self.frames[int(self.bird_index)]
     
     def update(self):
+        #Continuously update all functions for smooth gameplay
         self.player_input()
         self.apply_gravity()
         self.animation()
@@ -53,16 +59,17 @@ class Player(pygame.sprite.Sprite):
 class PipeDown(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        
+        #Import images
         pipe_down = pygame.image.load('desktop/Pygamefiles/Flappy Bird/Graphics/Pipes/Pipe_2_sprite.png').convert_alpha()
         self.image = pipe_down
 
+        #Randomly set y-value of pipe
         global down_y_pos
         down_y_pos= random.randint(-550, -150)
         self.rect = self.image.get_rect(midtop = (1150, down_y_pos))
 
     def update(self):
-        #Speed of pipe movement
+        #Speed of pipe movement (in Update b/c pipe must continuously move across screen)
         self.rect.x -= speed
         self.destroy()
 
@@ -76,6 +83,8 @@ class PipeUp(pygame.sprite.Sprite):
         super().__init__()
         pipe_up = pygame.image.load('desktop/Pygamefiles/Flappy Bird/Graphics/Pipes/Pipe_1_sprite.png').convert_alpha()
         self.image = pipe_up
+
+        #Y position of pipe based on chosen Y position of downward pipe + pipe distance value (to create space b/w pipes)
         up_y_pos = down_y_pos + pipe_distance
         self.rect = self.image.get_rect(midtop = (1150, up_y_pos))
 
@@ -89,54 +98,116 @@ class PipeUp(pygame.sprite.Sprite):
         if self.rect.x <= -100:
             self.kill()
 
-#def display_score():
+class ScoreCounter(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        #Create line to sit in empty space b/w two pipes to detect if player avoids hitting pipe = score increases
+        line = pygame.image.load('desktop/Pygamefiles/Flappy Bird/Graphics/Line.png').convert_alpha()
+        self.image = line
+        self.rect = self.image.get_rect(midtop = (1150, 0))
+        
+    def update(self):
+        #Speed of line equal to speed of pipes so they move together
+        self.rect.x -= speed
+        self.destroy()
 
+    def destroy(self):
+        #Destroy line once moved off screen
+        if self.rect.x <= -100:
+            self.kill()
+
+#Functions
+def display_score():
+    score = 0
+    #Collisions between player & score counter line
+    #Line will disappear after collision to ensure score only increases by ONE
+    collisions = pygame.sprite.spritecollide(bird.sprite, score_counter, True)
+    if collisions:
+        score += 1
+    #Set up score display on screen
+    score_surf = score_font.render(f'Score {score_display}', False, (64,64,64))
+    score_rect = score_surf.get_rect(center = (500,50))
+    screen.blit(score_surf,score_rect)
+    return score
+    
 def sprite_collisions():
+    #If player collides with pipes, game stops
     if pygame.sprite.spritecollide(bird.sprite, pipe_down, False) or pygame.sprite.spritecollide(bird.sprite, pipe_up, False):
+        #Empty all groups before game restarts so pipes & score counter respawn from right side of screen
         pipe_down.empty()
         pipe_up.empty()
-        return False
-    else: return True
+        score_counter.empty()
         
-
-#General startup
-
-#Initialize pygame settings
+        #Use return statements to change boolean of game_state
+        #Game_active = false once collision happens i.e. game will END 
+        return False
+    #If player hits ground, game stops
+    elif bird.sprite.rect.bottom == 580:
+        pipe_down.empty()
+        pipe_up.empty()
+        score_counter.empty()
+        return False
+    else: 
+        return True
+        
+#INITIALIZE PYGAME SETTINGS
 pygame.init()
+
+#screen size + screen name
 screen = pygame.display.set_mode((1000,600))
 pygame.display.set_caption('Flappy Bird')
+
+#Clock sets framerate of gameplay
 clock = pygame.time.Clock()
-test_font = pygame.font.Font('desktop/Pygamefiles/PygameIntro/font/Pixeltype.ttf', 50)
 
-#Set game state
+#fonts
+score_font = pygame.font.Font('desktop/Pygamefiles/Flappy Bird/Fonts/arcadeclassic/ARCADECLASSIC.TTF', 50)
+instruct_font = pygame.font.Font('desktop/Pygamefiles/Flappy Bird/Fonts/arcadeclassic/ARCADECLASSIC.TTF', 40)
+
+#GAME STATES
 game_active = False
-start_time = 0
-score = 0
+score_display = 0
 
-#Import music 
+#MUSIC
 
-#Groups --> These are your ACTUAL SPRITES
-#Use Group() when you want to have multiple of the same sprite on the screen
-#GroupSingle() used when only 1 of that sprite will be on the screen at a time
+#GROUPS
+
+#Groups contain Sprites i.e. can be called without specifying .sprite
+#Use Group() when you want MULTIPLE of a single sprite on the screen at a time
 pipe_down = pygame.sprite.Group()
 pipe_up = pygame.sprite.Group()
+score_counter = pygame.sprite.Group()
 
+#GroupSingle only contains a single sprite BUT must be called with .sprite as a result
+#Use GroupSingle() when you only want ONE single sprite on screen at a time e.g. player character
 bird = pygame.sprite.GroupSingle()
 #Add instance of player group i.e. bird 
 bird.add(Player())
 
 
-#Create game background & convert alpha lock to erase transparent box 
+#IMPORT IMAGES
+
+#Background/foreground
+#Convert alpha erases transparent box around png images
 sky_surf = pygame.image.load('desktop/Pygamefiles/Flappy Bird/Graphics/Background/Background_SimpleSky.png').convert_alpha()
 ground_surf = pygame.image.load('desktop/Pygamefiles/Flappy Bird/Graphics/Foreground/Ground_Smaller.png').convert_alpha()
 
-#Timer
-#Must add +1 since we already have an event as the main game (?) note* please rewatch this part of the video
-obstacle_timer = pygame.USEREVENT + 1
+#Score message
+score_surf = score_font.render(f'Score {score_display}', False,(64,64,64))
+score_rect = score_surf.get_rect(midtop = (500,50))
+
+#Logo and Bird for start screen
+logo_surf = pygame.image.load('desktop/Pygamefiles/Flappy Bird/Graphics/Logo Sprite.png').convert_alpha()
+bird_surf = pygame.image.load('desktop/Pygamefiles/Flappy Bird/Graphics/Bird/Bird 4.png').convert_alpha()
+
+#TIMERS
+#create specific event for intervals of obstacles to appear on screen
+#set_timer specifies how often we want each obstacles to appear (via milliseconds)
+obstacle_timer = pygame.USEREVENT
 pygame.time.set_timer(obstacle_timer, 1500)
 
 
-#While game is running
+#GAME LOOP
 while True:
     #For ALL events in game
     for event in pygame.event.get():
@@ -144,23 +215,33 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        #Add obstacles to group
         if game_active:
+            #Initialize obstacles into each group
             if event.type == obstacle_timer:
                 pipe_down.add(PipeDown())
                 pipe_up.add(PipeUp())
+                score_counter.add(ScoreCounter())
         else:
             #If you press the enter key down, the game will start
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
                 game_active = True
-        #Keyboard/mouse functionality
-    #ONLY when game is ACTIVE
+                #Reset score to 0
+                score_display = 0
+                #Bird starts at same spot on screen each time game restarts
+                #Gravity reset to 0
+                bird.sprite.rect.x = 50
+                bird.sprite.rect.y = 300
+                bird.sprite.gravity = 0
     if game_active:
+        #Line for score tracking behind EVERYTHING else (dont want player to see)
+        score_counter.draw(screen)
+        score_counter.update()   
+
         #Place background on screen
         screen.blit(sky_surf,(0,0))
         
         #draw obstacles on screen
-        #Spritegroup main function = draw + update all sprites
+        #Spritegroup main function = draw + update all sprites 
         pipe_down.draw(screen)
         pipe_down.update()
 
@@ -173,12 +254,36 @@ while True:
         #Place foreground in front of pipes
         screen.blit(ground_surf,(0,460))
         
-        #If game_active = true, include sprite_collisions
+        #If sprite collision occurs, we will return False and game will end
         game_active = sprite_collisions()
+
+        #Score will update on screen
+        score_display += display_score()
+    
     else: 
+        #Set up main menu screen
         screen.blit(sky_surf,(0,0))
         screen.blit(ground_surf,(0,460))
-        
+        screen.blit(logo_surf, (150,30))
+        screen.blit(bird_surf, (470,350))
+
+        #If player gets score 0 OR player just starts playing
+        #Display instructions
+        if score_display == 0:
+            instruct_surf = instruct_font.render(f'Press Up Key to Start', False, (64,64,64))
+            instruct_rect = instruct_surf.get_rect(midtop = (500, 450))
+            screen.blit(instruct_surf, instruct_rect)
+
+            instruct2_surf = instruct_font.render(f'Use Arrow Keys to Move', False, (64,64,64))
+            instruct2_rect = instruct2_surf.get_rect(midtop = (500, 500))
+            screen.blit(instruct2_surf, instruct2_rect)
+        #If player finishes round of game
+        #Display their score 
+        else:
+            score_surf = score_font.render(f'Score {score_display}', False,(64,64,64))
+            score_rect = score_surf.get_rect(midtop = (500,500))
+            screen.blit(score_surf, score_rect)
     #Update screen with components of active game 
     pygame.display.update()
+    #Game framerate
     clock.tick(60)
